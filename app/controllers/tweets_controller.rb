82e58dcda49bb7891419cfbce11a99eab21b21cb
MARTA <martariosrguez@hotmail.com>
2022-08-29 16:31:49 +0200

    ActiveStorage

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -47,6 +47,6 @@ class TweetsController < ApplicationController
   private
 
     def tweet_params
-      params.require(:tweet).permit(:message)
+      params.require(:tweet).permit(:message, :image)
     end
 end
